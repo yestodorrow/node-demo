a8b9f8f7760c898503b0b2cff5a2fb1ca1beddf7
Yestodorrow <power998@163.com>
2022-04-19 16:29:30 +0800

    test

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const tracer = require('dd-trace').init();
+let dotenv =  require('dotenv');
+dotenv.config('./env');
+// const tracer = require('dd-trace').init();
 var koa = require('koa');
 var app =new koa();
 const cors = require('koa-cors');
+
+console.log(process.env,33)
 app.use(cors());
+const { logger, accessLogger } = require('./logger');
+
+app.use(accessLogger());
 
 app.use((ctx)=>{
-    console.log(ctx.request.url)
+    console.log(ctx.request.url);
+    ctx.set("",)
     ctx.body={status:200,msg:'测试的返回数据',data:ctx.request.url};
 });
 
--- web/index.js
+++ web/index.js
@@ -1,7 +1,15 @@
 const tracer = require('dd-trace').init();
+let dotenv =  require('dotenv');
+dotenv.config('./env');
+console.log(process.env,333)
 var koa = require('koa');
 var app =new koa();
 
+const { logger, accessLogger } = require('./logger');
+// tracer.use('koa', {
+//     service: 'pg-cluster'
+//   })
+app.use(accessLogger());
 const static = require('koa-static');
 app.use(static(__dirname + '/static'));
 
